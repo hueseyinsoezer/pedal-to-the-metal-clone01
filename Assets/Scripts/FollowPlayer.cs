1efa5fa3c3fc82f8abc8f033966898071dca2c44
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-02 00:41:49 +0300

    +Updated camera's position to make an easier way to access the offset.

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+
     public GameObject player;
+
+    private Vector3 offset = new Vector3(0, 5, -7);
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = player.transform.position + new Vector3(0, 5, -7);
+        //Offset the camera behind the player by adding to the player's position.
+        this.transform.position = player.transform.position + offset;
     }
 }
