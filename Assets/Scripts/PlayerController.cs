5cb98b1761b463dcee771660d66a187cdd7b7758
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-01 23:42:26 +0300

    +Added commit for the transform-formula to understand it

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
     {
         //Move the vehicle forward
         //removed because of usage, but is similar: transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward*Time.deltaTime*20);
+        transform.Translate(Vector3.forward*Time.deltaTime*20);//With deltaTime we can control how much time is actually passing, this makes forward(0,0,20) for a second
         Debug.Log(transform.position);
                
     }
