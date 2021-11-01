f521eefd7070ddbb8e8ab5334a90127db1b2259f
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-02 00:32:03 +0300

    + Added a new script for position of camera to follow the vehicle as FollowPlayer + Added a new public gameobject variable for player + Attached vehicle as player from Main Camera Inspector + Updated position of main camera for better view of the game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,7 +18,6 @@ public class PlayerController : MonoBehaviour
         //Move the vehicle forward
         //removed because of usage, but is similar: transform.Translate(0, 0, 1);
         transform.Translate(Vector3.forward*Time.deltaTime*speed);//With deltaTime we can control how much time is actually passing, this makes forward(0,0,20) for a second
-        Debug.Log(transform.position);
                
     }
 }
