122ec5bed55212366995ecc3c22d3a843cf1ceb3
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-03 01:50:05 +0300

    +Updated horizontal speed from vehicle to move to forward or to backward

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float turnSpeed;
-    public float horizontalInput;
+    public float horizontalInput, verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -18,10 +18,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-
+        verticalInput = Input.GetAxis("Vertical");
         //Move the vehicle forward
         //removed because of usage, but is similar: transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward*Time.deltaTime*speed);//With deltaTime we can control how much time is actually passing, this makes forward(0,0,20) for a second
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*verticalInput);//With deltaTime we can control how much time is actually passing, this makes forward(0,0,20) for a second
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed* horizontalInput);   //Vector3.right equals (1,0,0), this formel means that our vehicle moves (turnSpeed,0,0) per second   
 
 
