e12e25c79716cf73f6ee8b783d215f2eabef27a1
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-02 20:13:17 +0300

    +Added left and right movement on vehicle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public float turnSpeed;
     public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,6 @@ public class PlayerController : MonoBehaviour
         //Move the vehicle forward
         //removed because of usage, but is similar: transform.Translate(0, 0, 1);
         transform.Translate(Vector3.forward*Time.deltaTime*speed);//With deltaTime we can control how much time is actually passing, this makes forward(0,0,20) for a second
-               
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);   //Vector3.right equals (1,0,0), this formel means that our vehicle moves (turnSpeed,0,0) per second   
     }
 }
